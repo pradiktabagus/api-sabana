976567b1dc3757f4d20c80d0d62e7a350ff6fc46
pradiktabagus <pradiktabagus@gmail.com>
2020-09-17 17:16:50 +0700

    coba

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use("/", indexRouter);
  * @Router - /user/*
  * @Method - *
  */
-app.use("/auth", userRouter);
+app.use("/api/auth", userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { check, validationResult} = require("express-validator");
+const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
-const auth = require("../middleware/auth")
+const auth = require("../middleware/auth");
 const User = require("../model/user");
 
 /**
@@ -13,75 +13,71 @@ const User = require("../model/user");
  */
 
 router.post(
-    "/signup",
-    [
-        check("username", "Please Enter a Valid Username")
-        .not()
-        .isEmpty(),
-        check("email", "Please enter a valid email").isEmail(),
-        check("password", "Please enter a valid password").isLength({
-            min: 6
-        })
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array()
-            });
-        }
-
-        const {
-            username,
-            email,
-            password
-        } = req.body;
-        try {
-            let user = await User.findOne({
-                email
-            });
-            if (user) {
-                return res.status(400).json({
-                    msg: "User Already Exists"
-                });
-            }
-
-            user = new User({
-                username,
-                email,
-                password
-            });
-
-            const salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(password, salt);
-
-            await user.save();
-
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            };
-
-            jwt.sign(
-                payload,
-                "randomString", {
-                    expiresIn: 10000
-                },
-                (err, token) => {
-                    if (err) throw err;
-                    res.status(200).json({
-                        id: user.id,
-                        username: user.username,
-                        email: user,email,
-                    });
-                }
-            );
-        } catch (err) {
-            console.log(err.message);
-            res.status(500).send("Error in Saving");
-        }
+  "/signup",
+  [
+    check("username", "Please Enter a Valid Username").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Please enter a valid password").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
     }
+
+    const { username, email, password } = req.body;
+    try {
+      let user = await User.findOne({
+        email,
+      });
+      if (user) {
+        return res.status(400).json({
+          msg: "User Already Exists",
+        });
+      }
+
+      user = new User({
+        username,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        "randomString",
+        {
+          expiresIn: 10000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({
+            id: user.id,
+            username: user.username,
+            email: user,
+            email,
+          });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Error in Saving");
+    }
+  }
 );
 
 /**
@@ -90,58 +86,62 @@ router.post(
  * @param - /auth/login
  */
 
-router.post('/login',[
-    check('email', 'Please enter valid email').isEmail(),
-    check('password', 'Please enter valid password').isLength({
-        min: 6
-    })
-], async (req, res) => {
-    const error = validationResult(req)
-    if(!error.isEmpty()){
-        return res.status(400).json({
-            errors: error.array()
-        });
+router.post(
+  "/login",
+  [
+    check("email", "Please enter valid email").isEmail(),
+    check("password", "Please enter valid password").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({
+        errors: error.array(),
+      });
     }
 
-    const {email, password} = req.body
+    const { email, password } = req.body;
     try {
-        let user = await User.findOne({
-            email
+      let user = await User.findOne({
+        email,
+      });
+      if (!user)
+        return res.status(400).json({
+          message: "User not exist",
         });
-        if(!user)
-            return res.status(400).json({
-                message: 'User not exist'
-            })
-        const isMatch = await bcrypt.compare(password, user.password);
-        if(!isMatch)
-            return res.status(400).json({
-                message: 'Incorect Password'
-            })
-        const payload = {
-            user: {
-                id: user.id
-            }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch)
+        return res.status(400).json({
+          message: "Incorect Password",
+        });
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        "randomString",
+        {
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({
+            token,
+          });
         }
-        jwt.sign(
-            payload,
-            "randomString",
-            {
-                expiresIn: 3600
-            },
-            (err, token) => {
-                if(err) throw err;
-                res.status(200).json({
-                    token
-                })
-            }
-        )
+      );
     } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            message: 'Internal Server Error'
-        })
+      console.error(error);
+      res.status(500).json({
+        message: "Internal Server Error",
+      });
     }
-})
+  }
+);
 
 /**
  * @method - GET
@@ -150,16 +150,16 @@ router.post('/login',[
  */
 
 router.get("/me", auth, async (req, res) => {
-    try {
-      // request.user is getting fetched from Middleware after token authentication
-      const user = await User.findById(req.user.id);
-      res.json({
-          username: user.username,
-          email: user.email,
-      });
-    } catch (e) {
-      res.send({ message: "Error in Fetching user" });
-    }
+  try {
+    // request.user is getting fetched from Middleware after token authentication
+    const user = await User.findById(req.user.id);
+    res.json({
+      username: user.username,
+      email: user.email,
+    });
+  } catch (e) {
+    res.send({ message: "Error in Fetching user" });
+  }
 });
 
 module.exports = router;
