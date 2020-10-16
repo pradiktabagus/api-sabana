de7643f518586057ebd4bd3306a82fb2eaf6b294
pradiktabagus <pradiktabagus@gmail.com>
2020-10-16 08:24:39 +0700

    abbort

--- app.js
+++ app.js
@@ -39,7 +39,9 @@ app.use("/api/auth", userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
